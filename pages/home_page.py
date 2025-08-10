a08935bcf9c851bdc2e8e52270bc595a257c3ca3
omri zaher <omriza5@gmail.com>
2025-08-10 14:30:13 +0300

    Fix selector for add_board_btn and reduce wait duration in create_board method

--- pages/home_page.py
+++ pages/home_page.py
@@ -11,7 +11,7 @@ class HomePage:
         self.password = "123456"
         self.add_board_link = (By.CLASS_NAME, "js-add-board")
         self.board_title_textbox = (By.CLASS_NAME, "js-new-board-title")
-        self.add_board_btn = (By.CSS_SELECTOR, "input.primary.wide[type='submit'][value='Create']")
+        self.add_board_btn = (By.CSS_SELECTOR, "input[value='Create']")
     
     def is_header_displayed(self):
         """
@@ -23,7 +23,7 @@ class HomePage:
     def create_board(self, board_title):
         # Explicitly wait for the "Add Board" link to be present in the DOM]
         try:     
-            wait = WebDriverWait(self.driver, 20)  # Wait up to 20 seconds
+            wait = WebDriverWait(self.driver, 10)  # Wait up to 20 seconds
             board_link = wait.until(EC.presence_of_element_located(self.add_board_link))
             board_link.click()
             
