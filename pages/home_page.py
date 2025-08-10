b476a12edefbc838345b1234eea3a09760df4ce0
omri zaher <omriza5@gmail.com>
2025-08-10 14:20:54 +0300

    Fix button value case in create_board method for consistency

--- pages/home_page.py
+++ pages/home_page.py
@@ -11,7 +11,7 @@ class HomePage:
         self.password = "123456"
         self.add_board_link = (By.CLASS_NAME, "js-add-board")
         self.board_title_textbox = (By.CLASS_NAME, "js-new-board-title")
-        self.add_board_btn = (By.CSS_SELECTOR, "input.primary.wide[type='submit'][value='Create']")
+        self.add_board_btn = (By.CSS_SELECTOR, "input.primary.wide[type='submit'][value='create']")
     
     def is_header_displayed(self):
         """
@@ -22,23 +22,17 @@ class HomePage:
     
     def create_board(self, board_title):
         # Explicitly wait for the "Add Board" link to be present in the DOM]
-        try:
-            print(f"******** FIRST")      
+        try:     
             wait = WebDriverWait(self.driver, 10)  # Wait up to 20 seconds
-            print(f"******** SECOND")
             board_link = wait.until(EC.presence_of_element_located(self.add_board_link))
-            print(f"******** THIRD")
             board_link.click()
             
             # Find and fill the board title field
-            print(f"******** FOURTH")
             board_title_field = self.driver.find_element(*self.board_title_textbox)
             board_title_field.clear()
-            print(f"******** FIFTH")
             board_title_field.send_keys(board_title)
             
             # Explicitly wait for the "Create" button to be present in the DOM
-            print(f"******** SIXTH")
             board_add_btn = wait.until(EC.presence_of_element_located(self.add_board_btn))
             print(f"******** SEVENTH")
             board_add_btn.click()
