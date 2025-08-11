c575cce5dd7e0a0b9e6bbc140ac538b247da92b3
omri zaher <omriza5@gmail.com>
2025-08-11 20:37:44 +0300

    Increase WebDriverWait duration in add_list method for improved reliability in locating the add list button

--- pages/board_page.py
+++ pages/board_page.py
@@ -23,7 +23,7 @@ class BoardPage:
         This method adds a new list to the board.
         """
         take_screenshot(self.driver, "before_add_list")
-        wait = WebDriverWait(self.driver, 20)
+        wait = WebDriverWait(self.driver, 50)
         add_list_btn = wait.until(EC.visibility_of_element_located(self.add_list_button))
         take_screenshot(self.driver, "after waiting for add_list_btn")
         add_list_btn.click()
