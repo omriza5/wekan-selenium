0762a125237af14eb0ac9de8ee416a31c68b32e9
omri zaher <omriza5@gmail.com>
2025-08-11 19:17:44 +0300

    Remove unnecessary blank line in board_page.py for cleaner code

--- pages/board_page.py
+++ pages/board_page.py
@@ -1,6 +1,5 @@
 from time import time
 from selenium.webdriver.common.by import By
-
 from utils.screenshot import take_screenshot
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
