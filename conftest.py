160b6d099c83f95432e5e21a3a9ec8491c9034dc
omri zaher <omriza5@gmail.com>
2025-09-24 15:16:32 +0300

    Refactor pytest_runtest_setup to consolidate allure suite and label assignments

--- conftest.py
+++ conftest.py
@@ -8,8 +8,7 @@ def pytest_runtest_setup(item):
     suite_name = Config.suite_name()
     browser = Config.browser()
     resolution = f"{Config.screen_width()}x{Config.screen_height()}"
-    if suite_name:
+    if suite_name and browser and resolution:
+        allure.dynamic.suite(f"{suite_name} [{browser} {resolution}]")
+    elif suite_name:
         allure.dynamic.suite(suite_name)
-    if browser and resolution:
-        allure.dynamic.label("browser", browser)
-        allure.dynamic.label("resolution", resolution)
